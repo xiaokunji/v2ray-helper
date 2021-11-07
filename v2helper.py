67da8e7bb03866d93d32a910365b349ecca0f42a
肖坤跻 <1767822853@qq.com>
2021-11-07 17:27:20 +0800

    自己的签到-去掉微信

--- v2helper.py
+++ v2helper.py
@@ -24,31 +24,32 @@ def main():
     s.headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
     }
-    url0 = f'https://forever.ypork.com/auth/login'
+    url0 = f'https://w1.v2free.net/auth/login'
     try:
         r = requests.get(url0, timeout=15)
     except requests.exceptions.RequestException as e:
         print("响应失败")
-        send_wechat("网站响应失败")
+        # send_wechat("网站响应失败")
         return
 
     headers0 = {
-        'origin': 'https://forever.ypork.com',
-        'referer' : 'https://forever.ypork.com/auth/login'
+        'origin': 'https://w1.v2free.net',
+        'referer' : 'https://w1.v2free.net/auth/login'
     }
     try:
         r0 = s.post(url0, data=fromdata, headers=headers0, timeout=15)
     except requests.exceptions.RequestException as e:
         print(e)
-        send_wechat("r0失败" + e)
+        # send_wechat("r0失败" + e)
         return
     if r0.status_code == 200:
         t = json.loads(r0.text)
         print(t['msg'])
     else:
-        send_wechat("登录失败")
+        print("登录失败")
+        # send_wechat("登录失败")
 
-    url2 = f"https://forever.ypork.com/user/checkin"
+    url2 = f"https://w1.v2free.net/user/checkin"
 
     r2 = s.post(url2, timeout=15)
     r2.raise_for_status()
@@ -57,7 +58,7 @@ def main():
         print(t["msg"])
     else:
         print("Error")
-        send_wechat("发生错误")
+        # send_wechat("发生错误")
         exit(100)
 
 if __name__ == "__main__":

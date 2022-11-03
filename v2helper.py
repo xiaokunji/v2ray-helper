5ef82e049fc3f269e5e7d7007cea5aff591f4604
xkj <1767822853@qq.com>
2022-11-04 00:40:23 +0800

    Update v2helper.py
    去掉微信并使用可访问的v2路径

--- v2helper.py
+++ v2helper.py
@@ -24,24 +24,24 @@ def main():
     s.headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
     }
-    url0 = f'https://forever.pork16.com/auth/login'
+    url0 = f'https://w1.v2free.net/auth/login'
     try:
         r = requests.get(url0, timeout=15)
     except requests.exceptions.RequestException as e:
         print("响应失败")
-        send_wechat("网站响应失败")
+        # send_wechat("网站响应失败")
         return
 
     headers0 = {
-        'origin': 'https://forever.pork16.com',
+        'origin': 'https://w1.v2free.net',
         # 'referer' : 'https://forever.ypork.com/auth/login'
-        'referer' : 'https://forever.pork16.com/auth/login' 
+        'referer' : 'https://w1.v2free.net/auth/login' 
     }
     try:
         r0 = s.post(url0, data=fromdata, headers=headers0, timeout=15)
     except requests.exceptions.RequestException as e:
         print(e)
-        send_wechat("r0失败" + e)
+        # send_wechat("r0失败" + e)
         return
     if r0.status_code == 200:
         t = json.loads(r0.text)
@@ -49,7 +49,7 @@ def main():
     else:
         send_wechat("登录失败")
 
-    url2 = f"https://forever.pork16.com/user/checkin"
+    url2 = f"https://w1.v2free.net/user/checkin"
 
     r2 = s.post(url2, timeout=15)
     r2.raise_for_status()
@@ -58,7 +58,7 @@ def main():
         print(t["msg"])
     else:
         print("Error")
-        send_wechat("发生错误")
+        # send_wechat("发生错误")
         exit(100)
 
 if __name__ == "__main__":
